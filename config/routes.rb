621f8f41121638cc4e2de1f3e955c10b250a30b3
Rey Valeza <reyvaleza@yahoo.ca>
2014-08-22 00:28:25 -0700

    removed all the local videos

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   get 'site/register'
 
   get 'site/login'
+  
+  get 'site/test'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
